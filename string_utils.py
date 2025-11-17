e1d4fb793c5e46a80bcdb460ca78f0b3911ee67e
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-17 18:41:46 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,24 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    n = -1
+    for i in range(len(formula)):
+        if formula[i].isdigit() == true:
+            n = i
+            break
+    if n == -1:
+        return (formula, 1)
+    m =formula(:n)
+    l =formula(n:)
+    return (m, l)
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    y = -1
+    for i in range(len(formula)):
+        if formula[i].isupper() == true:
+            y= i
+            break
+    if y == -1:
+        return (formula)
+    u = formula(:y)
+    t = formuls(y:)
+    return (u,t)
