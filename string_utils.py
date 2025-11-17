19753144d6ef72d0b52311523551b4627c453ca7
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-17 20:52:09 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -5,12 +5,12 @@ def split_at_first_digit(formula):
         if formula[i].isdigit() == true:
             n.append(i)
             n_num =+1
-    w = 0
+    w = ""
     if n_num == 0:
         return (formula, 1)
     for i in range(n.lenght):
-        print formula[w:i]
-        w =+1
+        w = w + (formula[w:i])
+        
 
 def split_before_each_uppercases(formula):
     y = []
@@ -19,12 +19,12 @@ def split_before_each_uppercases(formula):
         if formula[i].isupper() == true:
             y.append(i)
             y_num =+1
-    g = 0
+    g = ""
     if y_num == 0:
         return formula
     for i in range(y.lenght):
-        print formula[g:i]
-        g = g+1
+        g = g + (formula[g:i])
+        
     
  
 
