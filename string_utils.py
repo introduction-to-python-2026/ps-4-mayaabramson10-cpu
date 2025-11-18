909d2125210cc45f6b881c225f97a10a3297809f
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-18 17:35:07 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,15 +1,15 @@
 def split_at_first_digit(formula):
   digit_place = [0]
   digit_num = 0
-  for i in range(len(formula)):
-    if formula[i].isdigit()== True:
-      digit_place.append(i)
+  for index in range(len(formula)):
+    if formula[index].isdigit()== True:
+      digit_place.append(index)
       digit_num =+1
   
   if digit_num != 0:
       resoult = ""
       for place in range(len(digit_place)):
-        if place + 1 < len(formula):
+        if place + 1 < len(digit_place):
           resoult = resoult + " " + (formula[digit_place[place]:digit_place[place+1]])
         else:
           resoult = resoult + " " + (formula[digit_place[place]:])
@@ -24,23 +24,22 @@ def split_before_each_uppercases(formula):
     upper_place = [0]
     upper_num = 0
     for i in range(len(formula)):
-        if formula[i].isupper() == true:
+        if formula[i].isupper() == True:
             upper_place.append(i)
             upper_num =+1
     
     if upper_num != 0:
       k = ""
-      for i in range(len(upper_place)):
-        if i+1 < len(formula):
-          k = k + " " + (formula[upper_place[i]:upper_place[i+1]])
+      for g in range(len(upper_place)):
+        if g+1 < len(upper_place):
+          k = k + " " + (formula[upper_place[g]:upper_place[g +1]])
         else:
-          k = k + " " + (formula[upper_place[i]:])
+          k = k + " " + (formula[upper_place[g]:])
       print(k)
     else:
       print(formula)
-        
+
     
-        
     
  
 
