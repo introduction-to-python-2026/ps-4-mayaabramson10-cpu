14f37a3b176b38b784f76f13b01174fc4368c993
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-18 17:24:51 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,31 +1,45 @@
 def split_at_first_digit(formula):
-    n = []
-    n_num = 0
-    for i in range(len(formula)):
-        if formula[i].isdigit() == True:
-            n.append(i)
-            n_num =+1
-    w = ""
-    if n_num == 0:
-        return (formula, 1)
-    for i in range(n.lenght):
-        w = w + (formula[w:i])
-    print(w)
-        
+  digit_place = [0]
+  digit_num = 0
+  for i in range(len(formula)):
+    if formula[i].isdigit()== True:
+      digit_place.append(i)
+      digit_num =+1
+  
+  if digit_num != 0:
+      resoult = ""
+      for place in range(len(digit_place)):
+        if place + 1 < len(formula):
+          resoult = resoult + " " + (formula[digit_place[place]:digit_place[place+1]])
+        else:
+          resoult = resoult + " " + (formula[digit_place[place]:])
+      print (resoult)
+  else:
+    print (formula , 1) 
+
+  
+
 
 def split_before_each_uppercases(formula):
-    y = []
-    y_num = 0
+    upper_place = [0]
+    upper_num = 0
     for i in range(len(formula)):
-        if formula[i].isupper() == True:
-            y.append(i)
-            y_num =+1
-    g = ""
-    if y_num == 0:
-        return formula
-    for i in range(y.lenght):
-        g = g + (formula[g:i])
-    print (g)
+        if formula[i].isupper() == true:
+            upper_place.append(i)
+            upper_num =+1
+    
+    if upper_num != 0:
+      k = ""
+      for i in range(len(upper_place)):
+        if i+1 < len(formula):
+          k = k + " " + (formula[upper_place[i]:upper_place[i+1]])
+        else:
+          k = k + " " + (formula[upper_place[i]:])
+      print(k)
+    else:
+      print(formula)
+        
+    
         
     
  
