93950cd6168d2e042c59a99c3dfa1ed05deb1e49
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-19 16:24:40 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -6,14 +6,17 @@ def split_at_first_digit(formula):
   return formula, 1
 
 def split_before_each_uppercases(formula):
+  
     start = 0
-  list = []
-for i in range(1, len(formula)):
-  if formula[i].isupper():
-    list.append(formula[start:i])
-    start = i
-list.append(formula[start:])
-return list
+    list = []
+    for i in range(1, len(formula)):
+      
+      if formula[i].isupper():
+         
+        list.append(formula[start:i])
+        start = i
+    list.append(formula[start:])
+    return list
                 
       
    
