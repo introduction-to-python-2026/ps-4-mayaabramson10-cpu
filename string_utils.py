6366b577e996bd3662c48b824a359852036fc90c
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-21 14:23:37 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,10 +1,10 @@
-def split_at_digit(formula):
+def split_at_first_digit(formula):
     i = 0
     while i < len(formula) and not formula[i].isdigit():
-        i += 1
+      i += 1
     
     if i == len(formula):
-        return (formula, 1)
+      return (formula, 1)
         
     prefix = formula[:i]
     number = int(formula[i:])
@@ -16,10 +16,12 @@ def split_before_each_uppercase(formula):
     start = 0
     
     for i in range(1, len(formula)):
-        if formula[i].isupper():
-            segments.append(formula[start:i])
-            start = i
+      if formula[i].isupper():
+        segments.append(formula[start:i])
+        start = i
             
     segments.append(formula[start:])
     
     return segments
+
+
